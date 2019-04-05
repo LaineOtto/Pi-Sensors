1e81f3e07240c1717afed7c52f4754b50fdf9aa1
Cacoethes <12479475+LaineOtto@users.noreply.github.com>
2019-04-05 11:41:25 -0700

    Delete influxSetup.sh

--- influxSetup.sh
+++
@@ -1,7 +0,0 @@
-curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
-echo "deb https://repos.influxdata.com/debian stretch stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
-sudo apt update
-sudo apt install influxdb chronograf -y
-sudo systemctl enable influxdb
-sudo systemctl start influxdb
-influx -execute 'CREATE DATABASE sensors'
